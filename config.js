4c58733df7a5d39ec5917c40ad2d9bc5df0e0b92
davidenochk <davidenoch@outlook.com>
2017-05-08 00:43:36 +0530

    Azure db integration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var configs = {};
 configs.applicationPort = 3000;
 configs.dbName = 'DETODO';
-configs.dbHost = 'localhost';
-configs.dbPort = 4230;
+configs.dbHost = 'davidenoch:yINj3XUHRAvd71DcdUUldXkJrqtgHYBos0yMdEvOsJk5wLD7TcRbvX2U3jGalX7Z0kwEZhdLBkUfErmzgSVXYA==@davidenoch.documents.azure.com';
+configs.dbPort = '10250/?ssl=true';
 
 module.exports = configs;
