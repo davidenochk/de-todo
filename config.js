35fec78fe9a6ae2fd5fddb94da08f71d13b4927a
davidenochk <davidenoch@outlook.com>
2017-05-08 00:51:45 +0530

    Port change

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var configs = {};
-configs.applicationPort = 3000;
+configs.applicationPort = 80;
 configs.dbName = 'DETODO';
 configs.dbHost = 'davidenoch:yINj3XUHRAvd71DcdUUldXkJrqtgHYBos0yMdEvOsJk5wLD7TcRbvX2U3jGalX7Z0kwEZhdLBkUfErmzgSVXYA==@davidenoch.documents.azure.com';
 configs.dbPort = '10250/?ssl=true';
